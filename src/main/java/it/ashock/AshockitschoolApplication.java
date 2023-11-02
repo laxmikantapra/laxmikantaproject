dd29488d868f1eab36191bfaf79e7877a6a99093
laxmikanta <laxmikantapradhanbrm2@gmail.com>
2023-11-02 16:50:07 +0530

    first commit

---
+++ src/main/java/it/ashock/AshockitschoolApplication.java
@@ -0,0 +1,13 @@
+package it.ashock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshockitschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshockitschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/ashock/AshockitschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ashock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AshockitschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
